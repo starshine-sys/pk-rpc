b15ce0b2a02aeae82098fbe6d93b43963a84ea30
Emily <emily@zukrosi.org>
2020-11-19 13:46:04 +0100

    put client ID in config file instead of code, expose IDs in hover text

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const DiscordRPC = require('discord-rpc');
 const axios = require('axios');
+const defaultClientId = '757707416719851651';
+const config = require('./config.json');
+const clientId = config.clientId;
 
-const clientId = '757707416719851651';
 DiscordRPC.register(clientId);
-const client = new DiscordRPC.Client({transport: 'ipc'});
+const client = new DiscordRPC.Client({ transport: 'ipc' });
 
 axios.defaults.baseURL = 'https://api.pluralkit.me/v1';
 
@@ -11,31 +13,41 @@ var system;
 
 async function setFront() {
 	try {
-		if(!system) {
+		if (!system) {
 			var data = (await axios(`/a/${client.user.id}`)).data;
 			system = data;
 		}
 
 		var front = (await axios(`/s/${system.id}/fronters`)).data;
 
-		client.setActivity({
-			details: front.members.map(m => m.name).join(", ") || "(none)",
-			state: system.name || "---",
-			startTimestamp: new Date(front.timestamp),
-			//uncomment below if you want images & are using your own client
-			// largeImageKey: front.members[0]?.id || "none",
-			// largeImageText: front.members[0]?.name || "none",
-			// smallImageKey: system.id || "system",
-			// smallImageText: system.name || "system",
-			instance: false
-		})
-	} catch(e) {
-		if(e.response) {
-			if(e.response.data == "Account not found.") {
+		if (clientId == defaultClientId) {
+			activity = {
+				details: front.members.map(m => m.display_name || m.name).join(", ") || "(none)",
+				state: system.name || "---",
+				startTimestamp: new Date(front.timestamp)
+			};
+		} else {
+			activity = {
+				details: front.members.map(m => m.display_name || m.name).join(", ") || "(none)",
+				state: system.name || "---",
+				startTimestamp: new Date(front.timestamp),
+				//uncomment below if you want images & are using your own client
+				largeImageKey: front.members[0]?.id || "none",
+				largeImageText: "pk;m " + front.members[0]?.id || "none",
+				smallImageKey: system.id,
+				smallImageText: "pk;s " + system.id || "system",
+				instance: false
+			};
+		}
+
+		client.setActivity(activity)
+	} catch (e) {
+		if (e.response) {
+			if (e.response.data == "Account not found.") {
 				console.error("Account doesn't have a system registered.");
-			} else if(e.response.response == "Unauthorized to view fronter.") {
+			} else if (e.response.response == "Unauthorized to view fronter.") {
 				console.error("Account's front is set to private.");
-			} else if(e.response.response == "System has no registered switches.") {
+			} else if (e.response.response == "System has no registered switches.") {
 				console.error("Account has no registered switches.");
 			} else console.error(e.response.data);
 		} else console.error(e.message);
@@ -43,10 +55,10 @@ async function setFront() {
 	}
 }
 
-client.on('ready', ()=> {
+client.on('ready', () => {
 	setFront();
 
-	setInterval(()=> setFront(), 15000);
+	setInterval(() => setFront(), 15000);
 })
 
-client.login({clientId}).catch(console.error).then(()=> console.log("RPC running!"));
+client.login({ clientId }).catch(console.error).then(() => console.log("RPC running!"));
