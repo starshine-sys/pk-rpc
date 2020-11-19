fceee320de693460827c453c642576df7e3955ea
Emily <emily@zukrosi.org>
2020-11-19 15:49:44 +0100

    fallback to member name/cutoff in case details string is too long

--- index.js
+++ index.js
@@ -20,15 +20,22 @@ async function setFront() {
 
 		var front = (await axios(`/s/${system.id}/fronters`)).data;
 
+		members = front.members.map(m => m.display_name || m.name).join(", ");
+		if (members.length > 127) {
+			members = front.members.map(m => m.name).join(", ");
+			if (members.length > 127) {
+				members = members.slice(0, 120) + "...";
+			}
+		}
 		if (clientId == defaultClientId) {
 			activity = {
-				details: front.members.map(m => m.display_name || m.name).join(", ") || "(none)",
+				details: members || "(none)",
 				state: system.name || "---",
 				startTimestamp: new Date(front.timestamp)
 			};
 		} else {
 			activity = {
-				details: front.members.map(m => m.display_name || m.name).join(", ") || "(none)",
+				details: members || "(none)",
 				state: system.name || "---",
 				startTimestamp: new Date(front.timestamp),
 				//uncomment below if you want images & are using your own client
